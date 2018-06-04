ff6b8e4b8a9f772c3873f50a4dbb4eb12f64135c
Brian Downing <bdowning@lavos.net>
2018-06-04 02:16:16 -0500

    Conversion network and tests

--- conversion-network.ts
+++ conversion-network.ts
@@ -8,8 +8,54 @@ type IEdge = {
 export class UnitConversionNetwork {
     private edges: { [left: string]: { [right: string]: IEdge } } = { };
 
+    private addEdge(left: string, leftRate: Unit, right: string, rightRate: Unit) {
+        if (!this.edges[left])
+            this.edges[left] = { };
+        if (this.edges[left][right])
+            throw new Error('duplicate edge');
+        this.edges[left][right] = { leftRate, rightRate };
+    }
+
     addRate(num: number, numUnit: Unit, denom: number, denomUnit: Unit) {
+        this.rates = undefined;
         let numRate = numUnit.mul(Unit.one(num));
-           
+        let denomRate = denomUnit.mul(Unit.one(denom));
+        this.addEdge(numUnit.termsKey(), numRate, denomUnit.termsKey(), denomRate);
+        this.addEdge(denomUnit.termsKey(), denomRate, numUnit.termsKey(), numRate);
+    }
+
+    private rates?: { [key: string]: Unit | undefined };
+
+    private computeRates() {
+        if (this.rates)
+            return this.rates;
+        this.rates = { };
+        let rates = this.rates;
+        let visited: { [x: string]: boolean } = { };
+        let walk = (fromK: string, leftK: string, pathRate: Unit) => {
+            for (let rightK in this.edges[leftK]) {
+                if (rightK === fromK || visited[fromK + rightK])
+                    continue;
+                visited[fromK + rightK] = true;
+                let { leftRate, rightRate } = this.edges[leftK][rightK];
+                let rate = pathRate.mul(leftRate.div(rightRate));
+                if (rates[rate.termsKey()] != null)
+                    throw new Error('duplicate rate');
+                rates[rate.termsKey()] = rate;
+                walk(fromK, rightK, rate);
+            }
+        };
+        for (let fromK in this.edges)
+            walk(fromK, fromK, Unit.one());
+        return rates;
+    }
+
+    getConversionUnit(from: Unit, to: Unit) {
+        let rates = this.computeRates();
+        let rateU = to.div(from);
+        let rate = rates[rateU.termsKey()];
+        if (!rate)
+            throw new Error('no conversion');
+        return rate;
     }
 };
---
+++ tests/conversion-network.ts
@@ -0,0 +1,60 @@
+import { Unit, units } from '../index';
+import { Q, q } from '../quantity';
+import { UnitConversionNetwork } from '../conversion-network';
+import { expect } from 'chai';
+import 'mocha';
+
+let epsilon = 1e-6;
+
+describe('Conversion network', () => {
+    it('should be creatable', () => {
+        let net = new UnitConversionNetwork();
+    });
+
+    it('should be loadable with rates', () => {
+        let net = new UnitConversionNetwork();
+        net.addRate(42, Unit.base('thing'), 12, units.kilogram);
+        net.addRate(9, Unit.base('stuff'), 9, units.pound);
+    });
+
+    it('should fail when adding duplicate rates', () => {
+        let net = new UnitConversionNetwork();
+        net.addRate(42, Unit.base('thing'), 12, units.kilogram);
+        expect(() => net.addRate(9, Unit.base('thing'), 9, units.pound)).to.throw();
+    });
+
+    it('should produce a conversion rate', () => {
+        let net = new UnitConversionNetwork();
+        net.addRate(42, Unit.base('thing'), 12, units.kilogram);
+        net.addRate(1, Unit.base('stuff'), 9, units.pound);
+        let u = net.getConversionUnit(Unit.base('thing'), Unit.base('stuff'));
+        expect(u.termsKey()).to.equal('[0,0,0,0,0,0,0,0,0,"stuff",1,"thing",-1]');
+        let c = q(3.5, Unit.base('thing')).mul(q(1, u)).convert(Unit.base('stuff'));
+        expect(c.n).to.be.closeTo(1/4.082328, epsilon);
+    });
+
+    it('should produce all valid conversion rates', () => {
+        let net = new UnitConversionNetwork();
+        net.addRate(42, Unit.base('thing'), 12, units.kilogram);
+        net.addRate(1, Unit.base('stuff'), 9, units.pound);
+        net.getConversionUnit(Unit.base('thing'), Unit.base('stuff'));
+        net.getConversionUnit(Unit.base('stuff'), Unit.base('thing'));
+        net.getConversionUnit(units.kilogram, Unit.base('thing'));
+        net.getConversionUnit(units.kilogram, Unit.base('stuff'));
+        net.getConversionUnit(Unit.base('thing'), units.kilogram);
+        net.getConversionUnit(Unit.base('stuff'), units.kilogram);
+    });
+
+    it('should fail to convert where no continuity exists', () => {
+        let net = new UnitConversionNetwork();
+        net.addRate(42, Unit.base('thing'), 12, units.kilogram);
+        net.addRate(1, Unit.base('stuff'), 9, units.ampere);
+        expect(() => net.getConversionUnit(Unit.base('thing'), Unit.base('stuff'))).to.throw();
+        expect(() => net.getConversionUnit(Unit.base('stuff'), Unit.base('thing'))).to.throw();
+        expect(() => net.getConversionUnit(units.kilogram, Unit.base('thing'))).to.not.throw();
+        expect(() => net.getConversionUnit(units.kilogram, Unit.base('stuff'))).to.throw();
+        expect(() => net.getConversionUnit(Unit.base('thing'), units.kilogram)).to.not.throw();
+        expect(() => net.getConversionUnit(Unit.base('stuff'), units.kilogram)).to.throw();
+    });
+});
+
